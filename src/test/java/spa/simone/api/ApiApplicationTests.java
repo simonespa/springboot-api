018da84bb13d0a1a246f4e81d5a6a7cb6d1bb941
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2022-08-19 11:50:52 +0100

    Setup initial source code and gradle build

---
+++ src/test/java/spa/simone/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spa.simone.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
